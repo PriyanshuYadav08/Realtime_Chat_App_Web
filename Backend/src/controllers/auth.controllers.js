6aff70887412a0e91efee3320b31edb1be513b39
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-03-21 01:58:06 +0530

    api_check

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -1,11 +1,16 @@
 import User from "../models/user.models.js";
+import { generateToken } from "../lib/utils.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
 
-    const { email, password, fullName } = req.body;
+    const { email, fullName, password } = req.body;
     
     try{
+        if (!email || !password || !fullName) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+
         if (password.length < 7) {
             return res.status(400).json({message: "Password must be at least 7 characters long"});
         }
@@ -16,23 +21,28 @@ export const signup = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
-            email: email, 
-            password: hashedPassword, 
-            fullName: fullName
+            email,
+            fullName,
+            password: hashedPassword
         });
 
         if (newUser){
             //we can generate a JWT token here
+            const token = generateToken(newUser._id, res);
             await newUser.save();
-            res.status(201).json({message: "User Created Successfully"});
+            res.status(201).json({
+                _id: newUser._id,
+                email: newUser.email,
+                fullName: newUser.fullName,
+                profilePic: newUser.profilePic
+            });
         } else{
             res.status(400).json({message: "Invalid User Data"});
         }
 
-    } 
-    
-    catch (error) {
-        console.log(error);
+    } catch (error) {
+        console.log("Error in signing in: ", error);
+        res.status(500).json({message: "Server Error"});
     }
 };
 
--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -6,9 +6,13 @@ export const generateToken = (userId, res) => {
         expiresIn: "5d",
     });
 
-    res.cookie("token", token, {
+    res.cookie("token", token, { //this will be a httpOnly cookie, hence accessible only by the server
+        //and more secure
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 5 * 24 * 60 * 60 * 1000,
+        secure: process.env.NODE_ENV != "development",
+        maxAge: 5 * 24 * 60 * 60 * 1000, // 5 days in milliseconds
+        sameSite: "strict",
     });
+
+    return token;
 };
