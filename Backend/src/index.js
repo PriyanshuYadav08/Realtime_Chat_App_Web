c0229b87c6a4e27668f3b79bb4fbe5a6da43c9a5
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-03-27 01:28:36 +0530

    dp_update_Setup

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
