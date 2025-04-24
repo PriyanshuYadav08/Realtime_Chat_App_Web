e9e3245b0478469e0f4fb7d30d6878775890f0e6
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-04-24 13:48:19 +0530

    More updates related to frontend and loading

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,6 +4,7 @@ import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -12,6 +13,10 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -1,6 +1,24 @@
 import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios';
 
 export const useAuthStore = create((set) => ({
     authUser: null,
+
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
     isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({ authUser:res.data})
+        } catch (error) {
+            console.error("Error checking auth", error);
+            set({ authUser: null })
+        } finally {
+            set({ isCheckingAuth: false })
+        }
+    }
 }));
