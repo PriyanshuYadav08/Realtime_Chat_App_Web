71f9a91e28f69bafab7d098501d1d1666c7af7e0
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-04-24 12:46:02 +0530

    Updates for the pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,26 @@
+import NavBar from './components/NavBar';
+import { Routes,Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+import { useAuthStore } from './store/useAuthStore';
+
 const App =() => {
+  const {authUser} = useAuthStore()
+
   return (
     <div>
+      <NavBar/>
 
-    
-
+      <Routes>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/settings" element={<SettingsPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
     </div>
   );
 }
